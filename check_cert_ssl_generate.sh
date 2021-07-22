2de91437cbefed8d51ea03fac5da1f233dded281
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-07-22 17:58:37 +0700

    Update check_cert_ssl_generate.sh

--- check_cert_ssl_generate.sh
+++ check_cert_ssl_generate.sh
@@ -6,13 +6,13 @@ if [[ $# -eq 0 ]]
 then
 	echo "Arguments not given. Usage: ./checkcert.sh CERTIFICATE.crt PRIVKEY.key"
 else
-	crthash=$(openssl x509 -noout -modulus -in "$cert" | openssl md5)
-	echo $cert $crthash
-	keyhash=$(openssl rsa -noout -modulus -in "$key" | openssl md5)
+	crthash=$(openssl x509 -noout -modulus -in "$cert_ssl" | openssl md5)
+	echo $cert_ssl $crthash
+	keyhash=$(openssl rsa -noout -modulus -in "$key_ssl" | openssl md5)
 	if [ "$keyhash" = "$crthash" ]
 	then
-    	keytest=$(openssl rsa -in "$key" -check -noout)
-    	echo $key $keyhash
+    	keytest=$(openssl rsa -in "$key_ssl" -check -noout)
+    	echo $key_ssl $keyhash
     	echo "---- "$keytest" ----"
 	else
     	echo "!!!! Invalid key for given cert !!!!"
